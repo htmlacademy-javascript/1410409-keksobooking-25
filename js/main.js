ac4a7a7e8d318d54b8cbd302507c1a7df9ce383e
denispan <8888670@gmail.com>
2022-02-20 21:10:29 +0300

    исправление замечаний

--- js/main.js
+++ js/main.js
@@ -1,35 +1,20 @@
-function getrandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+const getRandomInt = (min, max) => {
+  const from = Math.ceil(min);
+  const to = Math.floor(max);
 
-  if (min < 0 || max < 0) {
-    return 'введен неверный диапазон -> диапазон может быть только положительный, включая ноль';
-  }
-  if (min === max) {
-    return min;
+  if (from >= to || from < 0) {
+    return RangeError('введен неверный диапазон');
   }
 
-  if (min > max) {
-    return Math.floor(Math.random() * (min - max + 1) + max);
+  return Math.floor(Math.random() * (to - from + 1) + from);
+};
+
+const getRandomFloat = (min = 1, max = 1, decimal = 5) => {
+  if (min >= max || max < 0 || decimal < 0) {
+    return RangeError('введено отрцательное значение диапазона');
   }
+  return +(Math.random() * (max - min) + min).toFixed(decimal);
+};
 
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function getRandomFloat (min = 1, max = 1, decimal = 0) {
-  if (min < 0 || max < 0 || decimal < 0) {
-    return 'введен неверный диапазон -> диапазон может быть только положительный, включая ноль';
-  }
-  if (min === max) {
-    return min.toFixed(decimal);
-  }
-
-  if (min > max) {
-    return (Math.random() * (min - max) + max).toFixed(decimal);
-  }
-
-  return (Math.random() * (max - min) + min).toFixed(decimal);
-}
-
-getrandomInt();
+getRandomInt();
 getRandomFloat();
