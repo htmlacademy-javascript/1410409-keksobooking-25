8a9b03858035f1e070584a145b42317cd0ca69f1
denispan <8888670@gmail.com>
2022-02-27 01:52:40 +0300

    temp

--- js/main.js
+++ js/main.js
@@ -16,5 +16,56 @@ const getRandomFloat = (min, max, decimal = 5) => {
   return Number((Math.random() * (max - min) + min).toFixed(decimal));
 };
 
-getRandomInt();
-getRandomFloat();
+//Создание массива с ссылками на автары пользователя
+
+// функция перемешивания элементов массива взята отсюда https://learn.javascript.ru/task/shuffle
+const shuffleArr = (array) => {
+  const arrTemp = array.slice();
+  for (let i = arrTemp.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arrTemp[i], arrTemp[j]] = [arrTemp[j], arrTemp[i]];
+  }
+  return arrTemp;
+};
+
+const getAvatarSrcImgArr = (countSrc) => {
+  const arr = [];
+  for (let i = 1; i <= countSrc; i++) {
+    if (i < 10) {
+      arr.push(`img/avatars/user0${  i  }.png`);
+    } else {
+      arr.push(`img/avatars/user${  i  }.png`);
+    }
+  }
+  return shuffleArr(arr);
+};
+
+const avatarSrcImgArr = getAvatarSrcImgArr(10);
+
+const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length -1)];
+
+const nearAd = {
+  author: {
+    avatar: getRandomArrayElement(avatarSrcImgArr)
+  },
+  offer: {
+    title: 'Шикарный гараж',
+    address: '35.65000, 139.70000',
+    price: 500,
+    type: 'palace',
+    rooms: 3,
+    guests: 3,
+    checkin: '14:00',
+    checkout: '12:00',
+    features: ['wifi', 'dishwasher', 'parking', 'washer'],
+    description: 'Лучший гараж для отдыха',
+    photos: [
+      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+    ],
+  },
+  location: {
+    lat: 35.70000,
+    lng: 139.80000
+  }
+};
