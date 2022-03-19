353bb5324d99603d585bd36a9c3b88f60c4ec46d
denispan <8888670@gmail.com>
2022-03-19 22:41:51 +0300

    Отрисуй меня полностью (часть 2)

--- js/form.js
+++ js/form.js
@@ -1,31 +1,30 @@
-const deactivateForm = () => {
-  const adForm = document.querySelector('.ad-form');
-  const mapFilters = document.querySelector('.map__filters');
+const adForm = document.querySelector('.ad-form');
+const mapFilters = document.querySelector('.map__filters');
 
-  adForm.classList.add('ad-form--disabled');
-  Array.from(adForm.children).forEach((element) => {
-    element.disabled = true;
-  });
-
-  mapFilters.classList.add('map__filters--disabled');
-  Array.from(mapFilters.children).forEach((element) => {
-    element.disabled = true;
+const toggleAttributeDisabled = (collection) => {
+  Array.from(collection).forEach((element) => {
+    if (element.disabled) {
+      element.disabled = false;
+    } else {
+      element.disabled = true;
+    }
   });
 };
 
-const activateForm = () => {
-  const adForm = document.querySelector('.ad-form');
-  const mapFilters = document.querySelector('.map__filters');
+const deactivateForm = () => {
+  adForm.classList.add('ad-form--disabled');
+  toggleAttributeDisabled(adForm.children);
 
+  mapFilters.classList.add('map__filters--disabled');
+  toggleAttributeDisabled(mapFilters.children);
+};
+
+const activateForm = () => {
   adForm.classList.remove('ad-form--disabled');
-  Array.from(adForm.children).forEach((element) => {
-    element.disabled = false;
-  });
+  toggleAttributeDisabled(adForm.children);
 
   mapFilters.classList.remove('map__filters--disabled');
-  Array.from(mapFilters.children).forEach((element) => {
-    element.disabled = false;
-  });
+  toggleAttributeDisabled(mapFilters.children);
 };
 
 export {deactivateForm, activateForm};
