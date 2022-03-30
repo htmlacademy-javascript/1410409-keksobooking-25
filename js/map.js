fc7ad3d43067739e29857a24bef35cacd178b6fa
denispan <8888670@gmail.com>
2022-03-31 00:24:51 +0300

    module9-task1

--- js/map.js
+++ js/map.js
@@ -1,26 +1,44 @@
 const TOKYO = {
-  lat: 35.681729,
-  lng: 139.753927,
+  lat: 35.68173,
+  lng: 139.75393,
 };
 
 const address = document.querySelector('#address');
 
-const mainPinIcon = L.icon({
+const pinIcoMain = L.icon({
   iconUrl: './img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
 
-const marker = L.marker(
+const pinIconCommon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+const markerMain = L.marker(
   {
     lat: TOKYO.lat,
     lng: TOKYO.lng,
   },
   {
     draggable: true,
-    icon: mainPinIcon,
+    icon: pinIcoMain,
   }
 );
+
+const markerCommon = L.marker(
+  {
+    lat: 35.56441,
+    lng: 139.99825,
+  },
+  {
+    draggable: true,
+    icon: pinIconCommon,
+  }
+);
+
 
 const createMap = (activateForm, initValidation) => {
   const map = L.map('map-canvas')
@@ -45,8 +63,12 @@ const createMap = (activateForm, initValidation) => {
     },
   ).addTo(map);
 
-  marker.addTo(map);
+  markerMain.addTo(map);
+  markerCommon.addTo(map);
 
+  markerMain.on('moveend', (evt) => {
+    address.value = `${(evt.target.getLatLng().lat).toFixed(5)}, ${evt.target.getLatLng().lng.toFixed(5)}`;
+  });
 
 
 
