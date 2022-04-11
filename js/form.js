673dab354077e31d6a520218905c671b2f5a991d
denispan <8888670@gmail.com>
2022-04-11 22:12:00 +0300

    module12-task1

--- js/form.js
+++ js/form.js
@@ -1,5 +1,4 @@
 import {resetMap} from './map.js';
-import {renderFilteredMarkers} from './map-filters.js';
 
 const adForm = document.querySelector('.ad-form');
 const mapFilters = document.querySelector('.map__filters');
